ee7902070eef99a87b394ff7f480a604402e08c7
Zino Meyer <zino.meyer@code.berlin>
2025-04-29 17:59:53 +0200

    chore: adapt copy

--- hooks/use-create-profile-mutation.tsx
+++ hooks/use-create-profile-mutation.tsx
@@ -13,7 +13,9 @@ export function useCreateProfileMutation() {
     },
     onError: (error) => {
       if (error.message === ApiErrorType.ResourceAlreadyExists) {
-        toast.error("Profile already exists. Try another link or username.");
+        toast.error(
+          "A profile for this username already exists. Try another link or username."
+        );
       } else if (error.message === ApiErrorType.ResourceNotFound) {
         toast.error(
           "Couldn't find this profile. Try another link or username."
