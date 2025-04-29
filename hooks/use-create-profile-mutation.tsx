ba0870e0e32ec60ce5ba810d344b80d0800ff647
Zino Meyer <zino.meyer@code.berlin>
2025-04-29 14:35:49 +0200

    fix: oncreate: not found error message

--- hooks/use-create-profile-mutation.tsx
+++ hooks/use-create-profile-mutation.tsx
@@ -13,12 +13,14 @@ export function useCreateProfileMutation() {
     },
     onError: (error) => {
       if (error.message === ApiErrorType.ResourceAlreadyExists) {
-        toast.error("Profile already exists. Try another username,");
+        toast.error("Profile already exists. Try another link or username.");
       } else if (error.message === ApiErrorType.ResourceNotFound) {
-        toast.error("Couldn't find this profile.");
+        toast.error(
+          "Couldn't find this profile. Try another link or username."
+        );
       } else {
         toast.error("Couldn't create profile: " + error.message);
       }
-    },
+    }
   });
 }
